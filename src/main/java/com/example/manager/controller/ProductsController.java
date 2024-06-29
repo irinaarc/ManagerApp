8d635141f01e0c8c91e3cfec8f056e070a947376
arcirina <irinaarc0712@gmail.com>
2024-06-29 10:37:45 +0500

    добавлена возможность создания нового товара

--- src/main/java/com/example/manager/controller/ProductsController.java
+++ src/main/java/com/example/manager/controller/ProductsController.java
@@ -1,9 +1,13 @@
 package com.example.manager.controller;
 
+import com.example.manager.controller.payload.NewProductPayload;
+import com.example.manager.entity.Product;
 import com.example.manager.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,9 +18,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class ProductsController {
     private final ProductService productService;
 
-    @RequestMapping(value = "list", method = RequestMethod.GET)
+    @GetMapping("list")
     public String getProductsList(Model model){
         model.addAttribute("products", this.productService.findAllProducts());
         return "catalogue/products/list";
+    }
+
+    @GetMapping("create")
+    public String getNewProductPage() {
+        return "catalogue/products/new_product";
+    }
+
+    @PostMapping("create")
+    public String createProduct(NewProductPayload payload){
+        Product product = this.productService.createProduct(payload.title(), payload.details());
+        return "redirect:/catalogue/products/list"; // перенаправление
     }
 }
